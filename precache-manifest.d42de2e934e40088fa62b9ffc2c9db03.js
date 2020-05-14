3f2c7d957bfba40c2d8b4cdbf4ef5c8f7b02053c
Pankaj Sanam <pankaj.sanam@gmail.com>
2020-05-14 10:11:32 +0530

    Updates

---
+++ precache-manifest.d42de2e934e40088fa62b9ffc2c9db03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543a7bc260657b2e1665494ed0afd27c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "53e7aee22e3869fcff70",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e7aee22e3869fcff70",
+    "url": "/robofriends/static/js/main.da978636.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
